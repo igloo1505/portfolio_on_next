a04cc569fd3e7e0a9344e4fce928141bf202625c
Andrew Iglinski <Andrew@igloodevelopment.dev>
2022-02-03 15:49:32 -0600

    added some box shadow to the animations on scroll to skills section.

--- animations/scrollTriggerFunctions.js
+++ animations/scrollTriggerFunctions.js
@@ -1,171 +1,180 @@
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 export const animateOnScroll = () => {
-  gsap.registerPlugin(ScrollTrigger);
-  //   gsap.set(".square-anim", {
-  //     transformStyle: "preserve-3d",
-  //     transformPerspective: 1000,
-  //   });
-  //   const child = document.querySelector(`.hero-design:nth-child(${c})`);
-  gsap.set(".squareFlip *", {
-    transformStyle: "preserve-3d",
-    backfaceVisibility: "hidden",
-    rotationY: -180,
-  });
-  let tl = gsap.timeline();
-  tl.from(".content", {
-    y: "-30%",
-    opacity: 0,
-    duration: 2,
-    ease: "Power4.easeOut",
-  });
-  tl.from(
-    ".staggeredEntrance",
-    {
-      y: "-50",
-      opacity: 0,
-      stagger: 0.3,
-      duration: 2,
-      ease: "Power4.easeOut",
-    },
-    "-=1.5"
-  );
+	gsap.registerPlugin(ScrollTrigger);
+	//   gsap.set(".square-anim", {
+	//     transformStyle: "preserve-3d",
+	//     transformPerspective: 1000,
+	//   });
+	//   const child = document.querySelector(`.hero-design:nth-child(${c})`);
+	gsap.set(".squareFlip *", {
+		transformStyle: "preserve-3d",
+		backfaceVisibility: "hidden",
+		rotationY: -180,
+	});
+	let tl = gsap.timeline();
+	tl.from(".content", {
+		y: "-30%",
+		opacity: 0,
+		duration: 2,
+		ease: "Power4.easeOut",
+	});
+	tl.from(
+		".staggeredEntrance",
+		{
+			y: "-50",
+			opacity: 0,
+			stagger: 0.3,
+			duration: 2,
+			ease: "Power4.easeOut",
+		},
+		"-=1.5"
+	);
 
-  tl.from(
-    ".hero-design",
-    {
-      opacity: 0,
-      y: 50,
-      ease: "Power4.easeOut",
-      duration: 1,
-    },
-    "-=2"
-  );
+	tl.from(
+		".hero-design",
+		{
+			opacity: 0,
+			y: 50,
+			ease: "Power4.easeOut",
+			duration: 1,
+		},
+		"-=2"
+	);
 
-  gsap.from(".square-anim", {
-    stagger: 0.2,
-    scale: 0.1,
-    duration: 2.5,
-    // ease: "back.out(1.7)"
-    ease: "elastic.out(1, 0.3)",
-  });
+	gsap.from(".square-anim", {
+		stagger: 0.2,
+		scale: 0.1,
+		duration: 2.5,
+		// ease: "back.out(1.7)"
+		ease: "elastic.out(1, 0.3)",
+	});
 
-  gsap.from(".squareFlip", {
-    stagger: 0.2,
-    scale: 0.1,
-    duration: 2.5,
-    // ease: "back.out(1.7)"
-    ease: "elastic.out(1, 0.3)",
-  });
+	gsap.from(".squareFlip", {
+		stagger: 0.2,
+		scale: 0.1,
+		duration: 2.5,
+		// ease: "back.out(1.7)"
+		ease: "elastic.out(1, 0.3)",
+	});
 
-  gsap.from(".transition2", {
-    scrollTrigger: {
-      trigger: ".transition2",
-      start: "top 85%",
-    },
-    y: 50,
-    opacity: 0,
-    duration: 1.2,
-    stagger: 0.3,
-  });
+	gsap.from(".transition2", {
+		scrollTrigger: {
+			trigger: ".transition2",
+			start: "top 85%",
+		},
+		y: 50,
+		opacity: 0,
+		duration: 1.2,
+		stagger: 0.3,
+	});
+	let cardTL = gsap.timeline();
 
-  gsap.from(".transition3", {
-    scrollTrigger: {
-      trigger: ".transition3",
-      start: "top 85%",
-    },
-    y: 50,
-    opacity: 0,
-    duration: 1.2,
-    stagger: 0.3,
-  });
+	cardTL.from(".transition3", {
+		scrollTrigger: {
+			trigger: ".transition3",
+			start: "top 85%",
+		},
+		y: 50,
+		opacity: 0,
+		duration: 1.2,
+		stagger: 0.3,
+	});
 
-  gsap.from(".portfolio-left-4", {
-    scrollTrigger: {
-      trigger: ".transitionLeft4",
-      start: "top 90%",
-    },
-    x: -100,
-    opacity: 0,
-    duration: 1.2,
-    stagger: 0.3,
-  });
-  gsap.from(".transitionRight4", {
-    scrollTrigger: {
-      trigger: ".transitionRight4",
-      start: "top 90%",
-    },
-    x: 100,
-    opacity: 0,
-    duration: 1.2,
-    stagger: 0.3,
-  });
+	cardTL.to(".cardOuter", {
+		boxShadow:
+			"12px 12px 20px rgba(150, 170, 180, 0.5), -12px -12px 20px #e0e0e0",
+		transition: "all 0.3s ease-in-out",
+		stagger: 0.3,
+		// box-shadow: 8px 8px 12px rgba(150, 170, 180, 0.5), -8px -8px 12px #e0e0e0;
+	});
 
-  gsap.from(".portfolio-left-5", {
-    scrollTrigger: {
-      trigger: ".transitionLeft5",
-      start: "top 90%",
-    },
-    x: -100,
-    opacity: 0,
-    duration: 1.2,
-    stagger: 0.3,
-  });
-  gsap.from(".transitionRight5", {
-    scrollTrigger: {
-      trigger: ".transitionRight5",
-      start: "top 90%",
-    },
-    x: 100,
-    opacity: 0,
-    duration: 1.2,
-    stagger: 0.3,
-  });
+	gsap.from(".portfolio-left-4", {
+		scrollTrigger: {
+			trigger: ".transitionLeft4",
+			start: "top 90%",
+		},
+		x: -100,
+		opacity: 0,
+		duration: 1.2,
+		stagger: 0.3,
+	});
+	gsap.from(".transitionRight4", {
+		scrollTrigger: {
+			trigger: ".transitionRight4",
+			start: "top 90%",
+		},
+		x: 100,
+		opacity: 0,
+		duration: 1.2,
+		stagger: 0.3,
+	});
 
-  gsap.from(".portfolio-left-6", {
-    scrollTrigger: {
-      trigger: ".transitionLeft6",
-      start: "top 90%",
-    },
-    x: -100,
-    opacity: 0,
-    duration: 1.2,
-    stagger: 0.3,
-  });
+	gsap.from(".portfolio-left-5", {
+		scrollTrigger: {
+			trigger: ".transitionLeft5",
+			start: "top 90%",
+		},
+		x: -100,
+		opacity: 0,
+		duration: 1.2,
+		stagger: 0.3,
+	});
+	gsap.from(".transitionRight5", {
+		scrollTrigger: {
+			trigger: ".transitionRight5",
+			start: "top 90%",
+		},
+		x: 100,
+		opacity: 0,
+		duration: 1.2,
+		stagger: 0.3,
+	});
 
-  gsap.from(".transitionRight6", {
-    scrollTrigger: {
-      trigger: ".transitionRight6",
-      start: "top 90%",
-    },
-    x: 100,
-    opacity: 0,
-    duration: 1.2,
-    stagger: 0.3,
-  });
+	gsap.from(".portfolio-left-6", {
+		scrollTrigger: {
+			trigger: ".transitionLeft6",
+			start: "top 90%",
+		},
+		x: -100,
+		opacity: 0,
+		duration: 1.2,
+		stagger: 0.3,
+	});
 
-  ScrollTrigger.create({
-    start: 0,
-    onUpdate: (self) => {
-      let y = document.scrollingElement.scrollTop;
-      let Y = window.pageYOffset;
-      let emHeight = 434;
-      let scaleOnScroll = (emHeight - Y) / emHeight;
-      gsap.to(".square-anim", {
-        // rotation: self.progress * 850,
-        // duration: 1,
-        // rotationY: 180,
-        // rotationY: 180,
-        scaleX: scaleOnScroll,
-        scaleY: scaleOnScroll,
-        ease: "expo.easeOut",
-      });
+	gsap.from(".transitionRight6", {
+		scrollTrigger: {
+			trigger: ".transitionRight6",
+			start: "top 90%",
+		},
+		x: 100,
+		opacity: 0,
+		duration: 1.2,
+		stagger: 0.3,
+	});
 
-      gsap.to(".circleFadeOut", {
-        scaleX: scaleOnScroll,
-        scaleY: scaleOnScroll,
-      });
-    },
-  });
+	ScrollTrigger.create({
+		start: 0,
+		onUpdate: (self) => {
+			let y = document.scrollingElement.scrollTop;
+			let Y = window.pageYOffset;
+			let emHeight = 434;
+			let scaleOnScroll = (emHeight - Y) / emHeight;
+			gsap.to(".square-anim", {
+				// rotation: self.progress * 850,
+				// duration: 1,
+				// rotationY: 180,
+				// rotationY: 180,
+				scaleX: scaleOnScroll,
+				scaleY: scaleOnScroll,
+				ease: "expo.easeOut",
+			});
+
+			gsap.to(".circleFadeOut", {
+				scaleX: scaleOnScroll,
+				scaleY: scaleOnScroll,
+			});
+		},
+	});
 };
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import "../css/MaterializeModal.css";
 import "../css/App.css";
 // import ContactModal from "../components/NewContactModal";
+import ReactGA from "react-ga";
+import ttiPolyfill from "tti-polyfill";
 import ContactModal from "../components/ContactModal";
 import Navbar from "../components/Navbar";
 import Drawer from "../components/Drawer";
@@ -9,12 +11,77 @@ import { Provider } from "react-redux";
 import store from "../state/store";
 import gsap from "gsap";
 import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { MotionPathPlugin } from "gsap/dist/MotionPathPlugin";
+import { EaselPlugin } from "gsap/dist/EaselPlugin";
+import { animateOnScroll } from "../animations/scrollTriggerFunctions";
 // import M from "materialize-css/dist/js/materialize.min.js";
-import dynamic from "next/dynamic";
 
 function MyApp({ Component, pageProps }) {
-	gsap.registerPlugin(ScrollToPlugin);
+	gsap.registerPlugin(
+		EaselPlugin,
+		MotionPathPlugin,
+		ScrollToPlugin,
+		ScrollTrigger
+	);
 
+	function handlePerformance(list) {
+		list.getEntries().forEach((entry) => {
+			ReactGA.timing({
+				category: "Load Performance",
+				variable: "Server Latency",
+				value: entry.responseStart - entry.requestStart,
+			});
+			ReactGA.timing({
+				category: "Load Performance",
+				variable: "Download Time",
+				value: entry.responseEnd - entry.responseStart,
+			});
+			ReactGA.timing({
+				category: "Load Performance",
+				variable: "Total App Load Time",
+				value: entry.responseEnd - entry.requestStart,
+			});
+		});
+	}
+
+	useEffect(() => {
+		ReactGA.initialize([
+			{
+				trackingId: "G-YJK7R2Y1TL",
+				gaOptions: {
+					siteSpeedSampleRate: 100,
+					name: "GAtracker",
+					cookieDomain: "none",
+					storage: "none",
+				},
+			},
+			{
+				trackingId: "UA-181494074-1",
+				standardImplementation: true,
+				gaOptions: {
+					siteSpeedSampleRate: 100,
+					name: "OriginalTracker",
+					cookieDomain: "none",
+					storage: "none",
+				},
+			},
+		]);
+		var observer = new PerformanceObserver(handlePerformance);
+		observer.observe({ entryTypes: ["navigation"] });
+	}, []);
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			animateOnScroll();
+			ttiPolyfill.getFirstConsistentlyInteractive().then((tti) => {
+				ReactGA.timing({
+					category: "Load Performance",
+					variable: "Time to Interactive",
+					value: tti,
+				});
+			});
+		}
+	}, []);
 	return (
 		<>
 			<Provider store={store}>
