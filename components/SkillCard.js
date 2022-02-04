ecb826730f7d3e5ca7c4460f7e520d2c26a934a2
Andrew Iglinski <Andrew@igloodevelopment.dev>
2022-02-04 06:50:33 -0600

    Fixed overflow issue with skillCard that was coming from a completely unnecessary div. Add a function to have the boxShadow peak at 50vh exactly, which should work for both mobile and desktop devices cosmetically without a whole lot of change

--- components/SkillCard.js
+++ components/SkillCard.js
@@ -12,18 +12,16 @@ import {
 
 const SkillCard = ({ skill }) => {
 	return (
-		<div className="cardOuter">
-			<li key={skill.title} className="transition3">
-				<div className={`icon-container ${skill.classIndex}`}>
-					{skill.type === "MobileDevelopment" && swiftSvg()}
-					{skill.type === "FrontendDesign" && <TechIcons />}
-					{skill.type === "BusinessTechnologies" && googleAnalyticsSvg(80, 80)}
-					{skill.type === "secureBackend" && jwtSvg("auto", 80)}
-				</div>
-				<p className="skill-title">{skill.title}</p>
-				<p className="featured-desc skill-desc">{skill.desc}</p>
-			</li>
-		</div>
+		<li key={skill.title} className="transition3">
+			<div className={`icon-container ${skill.classIndex}`}>
+				{skill.type === "MobileDevelopment" && swiftSvg()}
+				{skill.type === "FrontendDesign" && <TechIcons />}
+				{skill.type === "BusinessTechnologies" && googleAnalyticsSvg(80, 80)}
+				{skill.type === "secureBackend" && jwtSvg("auto", 80)}
+			</div>
+			<p className="skill-title">{skill.title}</p>
+			<p className="featured-desc skill-desc">{skill.desc}</p>
+		</li>
 	);
 };
 
